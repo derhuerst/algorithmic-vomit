73edd3eb15b2d044943a8b71eb22fa0643373259
Jannis R <mail@jannisr.de>
2019-02-01 23:50:55 +0100

    worley-noise@2

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const size   = require('window-size')
 const Noise  = require('simplex-noise')
 const Worley = require('worley-noise')
 const noise  = new Noise(() => Math.random())
-const worley = new Worley(10)
+const worley = new Worley({numPoints: 10})
 
 const fill   = require('./index')
 
@@ -16,7 +16,7 @@ const patterns = {
 	, vertical:   (x, y) => y / (size.height - 1)
 	, simplex:    (x, y) => Math.abs(noise.noise2D(x/40, y/50))
 	, crazy:      (x, y) => Math.abs(noise.noise2D(x/35, y/35))
-	, worley:     (x, y) => Math.abs(worley.getManhattan(x/40, y/45, 2)/3)
+	, worley: (x, y) => Math.abs(worley.getManhattan({x: x / 40, y: y / 45}, 2) / 3)
 	, stripes:    (x, y) => {
 		x /= size.width - 1
 		y /= size.height - 1
